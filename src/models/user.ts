96040b26b6ca6376cc8344c8ef3682d93dd173dd
rupesh <rpiwal02@gmail.com>
2025-02-05 18:48:40 +0530

    feat: create UserSchema for user model

---
+++ src/models/user.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    selectedCategories: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
