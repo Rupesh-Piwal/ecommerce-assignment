c0651726ad1b8d86468eae60bf771f3c89fd4715
rupesh <rpiwal02@gmail.com>
2025-02-06 01:35:52 +0530

    setup MongoDB connection with env config

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+const dbConnect = async (): Promise<void> => {
+  if (connection.isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+    process.exit(1);
+  }
+};
+
+export default dbConnect;
